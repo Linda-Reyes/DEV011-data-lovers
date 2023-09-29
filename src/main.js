087b8046961d67add3daf44cd43e368877423d58
barbara-contreras <bcontreraslagos@gmail.com>
2023-09-28 22:51:44 -0300

    función medalla finalizada

--- src/main.js
+++ src/main.js
@@ -1,22 +1,60 @@
-import { example } from './dataFunctions.js';
+import { filterBy } from './dataFunctions.js';
 import { renderItems } from './view.js';
 import data from './data/athletes/athletes.js';
 
 //console.log(example, renderItems, data);//
 
+//Visualizar tarjetas
 const root = document.getElementById("root");
 root.appendChild(renderItems(data.athletes));
 
+//Función que filtra por medallas
 
+//athletesArray(data.athletes);
 
+//filterBy(data.athletes, "medal", "Gold")
+//filterBy(data.athletes, "medal", "Silver")
+//filterBy(data.athletes, "medal", "Bronze")
+
+//console.log(filterBy)
+
+const selectMedal = document.querySelector ('select[name="medal"][data-testid="medal-filter"]');
+//const medalGold = document.querySelector ('option[value="Gold"]');
+
+selectMedal.addEventListener('change', (e) => {
+  //llamando a función filtro medallas
+  const filterGold = filterBy(data.athletes, "medal", e.target.value);
+  root.innerHTML = "";
+  root.appendChild(renderItems(filterGold));
+ 
+});
+
+//Interacción y eventos para menú responsive
 const nav = document.querySelector("#nav");
 const open = document.querySelector("#open");
 const close = document.querySelector("#close");
 
 open.addEventListener("click", () => {
-    nav.classList.add("visible");
+  nav.classList.add("visible");
 })
 
 close.addEventListener("click", () => {
-    nav.classList.remove("visible");
+  nav.classList.remove("visible");
 })
+
+/*
+const medalSelect = document.querySelector('select [name="medal"] [data-testid="medal-select"]');
+
+medalSelect.addEventListener('change', () => {
+
+  // Llama a la función correspondiente dependiendo de la medalla seleccionada
+ 
+  if (selectedMedal === "Gold") {
+    filterBy(data.athletes, "medal", "Gold");
+  } else if (selectedMedal === "Silver") {
+    filterBy(data.athletes, "medal", "Silver");
+  } else if (selectedMedal === "Bronze") {
+    filterBy(data.athletes, "medal", "Bronze");
+  }
+  filterBy.innerHTML = 
+});*/
--- test/data.js
+++ test/data.js
@@ -1,5 +1,40 @@
 // Aqui puedes agregar los datos que necesites para tus pruebas
 // al menos debe tener uno que se llame "data"
 
-export const data = [];
+export const data = [ {
+  "name": "Giovanni Abagnale",
+  "gender": "M",
+  "height": "198",
+  "weight": "90",
+  "sport": "Rowing",
+  "team": "Italy",
+  "noc": "ITA",
+  "age": 21,
+  "event": "Rowing Men's Coxless Pairs",
+  "medal": "Bronze"
+},
+{
+  "name": "Patimat Abakarova",
+  "gender": "F",
+  "height": "165",
+  "weight": "49",
+  "sport": "Taekwondo",
+  "team": "Azerbaijan",
+  "noc": "AZE",
+  "age": 21,
+  "event": "Taekwondo Women's Flyweight",
+  "medal": "Bronze"
+},
+{
+  "name": "Luc Abalo",
+  "gender": "M",
+  "height": "182",
+  "weight": "86",
+  "sport": "Handball",
+  "team": "France",
+  "noc": "FRA",
+  "age": 31,
+  "event": "Handball Men's Handball",
+  "medal": "Silver"
+},];
 
