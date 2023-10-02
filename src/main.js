0b02461fae2be2a0e05c745a62f7cab46fdb2bea
Linda Reyes <lindamreyesr@gmail.com>
2023-10-02 09:46:10 -0500

    Funcion Sort y  clear

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { filterBy } from './dataFunctions.js';
+import { filterBy, sortAtheltes} from './dataFunctions.js';
 import { renderItems } from './view.js';
 import data from './data/athletes/athletes.js';
 
@@ -8,21 +8,46 @@ import data from './data/athletes/athletes.js';
 const root = document.getElementById("root");
 root.appendChild(renderItems(data.athletes));
 
+
+const dataAthletes = data.athletes;
 const selectMedal = document.querySelector ('select[name="medal"][data-testid="medal-filter"]');
-const genderSelect = document.querySelector("select[data-testid='gender-filter']");
+const genderSelect = document.querySelector('select[data-testid="gender-filter"]');
+const selectSort = document.querySelector('select[id="select-sort"][name="sort"][data-testid="select-sort"]');
+const ascSort = document.querySelector('option[value="asc"]');
+const resetButton = document.getElementById('clear');
+let filteredAthletes = [...dataAthletes];
 
 selectMedal.addEventListener('change', (e) => {
   //llamando a función filtro medallas
-  const filterMedal = filterBy(data.athletes, "medal", e.target.value);
+  const filterMedal = filterBy(filteredAthletes, "medal", e.target.value);
   root.innerHTML = "";
-  root.appendChild(renderItems(filterMedal));
+  filteredAthletes = filterMedal;
+  root.appendChild(renderItems(filteredAthletes));
 });
 
 genderSelect.addEventListener("change",(e) => {
   console.log(e.target.value);
-  const filterGender = filterBy(data.athletes,"gender",e.target.value);
+  const filterGender = filterBy(filteredAthletes,"gender",e.target.value);
   root.innerHTML = "";
-  root.appendChild (renderItems(filterGender));
+  filteredAthletes = filterGender;
+  root.appendChild (renderItems(filteredAthletes));
+});
+
+
+selectSort.addEventListener("change",(e) => {
+  console.log(e.target.value);
+  const generalSort = sortAtheltes(filteredAthletes,"name",e.target.value);
+  root.innerHTML = "";
+  filteredAthletes = generalSort
+  root.appendChild (renderItems(filteredAthletes));
+});
+
+resetButton.addEventListener("click",(e) => {
+  root.innerHTML = "";
+  filteredAthletes = [...dataAthletes];
+  root.appendChild (renderItems(filteredAthletes));
+  selectMedal.value = "default";
+  genderSelect.value = "default";
 });
 
 //Interacción y eventos para menú responsive
