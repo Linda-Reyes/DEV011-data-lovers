86fb7ce02a0dd1cdc13f3f4bf7c51a5fee656ea0
barbara-contreras <bcontreraslagos@gmail.com>
2023-10-06 23:42:12 -0300

    proyecto casi terminado

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-
-/*export const athletesArray = (data) => {
-  const newArr = data.map(pais => pais.team);
-  return newArr
-}*/
-
-export const filterBy = (data, filterBy, value) => {
-  const arrayFilter = data.filter(athlete => athlete[filterBy] === value);
-  return arrayFilter;
-};
-=======
 export const filterBy = ((data, filterBy, value) => {
   const arrayFilter = data.filter(athlete => {
     if (athlete[filterBy] === value) {
@@ -18,7 +6,6 @@ export const filterBy = ((data, filterBy, value) => {
   })
   return arrayFilter
 })
->>>>>>> 16ba1c1f24e31a4c215ff6bddbd77d79162fd9ed
 
 export const sortAtheltes = (data, sortBy, sortOrden) => {
   let arraySort = [];
@@ -44,9 +31,5 @@ export const computeStat = (data) => {
   const totalAthletes = ages.length;
   const sumAges = ages.reduce((acumulator, age) => acumulator + age, 0);
   const averageAge = sumAges / totalAthletes;
-<<<<<<< HEAD
-  return parseFloat (averageAge.toFixed(1));
-=======
   return parseFloat(averageAge.toFixed(1));
->>>>>>> 16ba1c1f24e31a4c215ff6bddbd77d79162fd9ed
 }
--- test/dataFunctions.spec.js
+++ test/dataFunctions.spec.js
@@ -1,66 +1,3 @@
-<<<<<<< HEAD
-import { filterBy, sortAtheltes } from '../src/dataFunctions.js';
-
-//TEST UNITARIO FUNCIÓN FILTRO
-describe('filterBy', () => {
-
-  it('que filtre por medalla seleccionada y no por otra', () => {
-    const lista = [
-      { medal: "Gold"},  
-      { medal: "Silver"},
-      { medal: "Bronze"}
-    ]
-    const arrayFilter = [
-      { medal: "Gold"}
-    ]
-    expect(filterBy(lista, "medal", "Gold")).toEqual(arrayFilter);
-  });
-
-  it('que filtre por medalla seleccionada y no por otra', () => {
-    const lista = [
-      { medal: "Gold"},  
-      { medal: "Silver"},
-      { medal: "Bronze"}
-    ]
-    const arrayFilter = [
-      { medal: "Silver"}
-    ]
-    expect(filterBy(lista, "medal", "Silver")).toEqual(arrayFilter);
-  });
-
-  it('que filtre por medalla seleccionada y no por otra', () => {
-    const lista = [
-      { medal: "Gold"},  
-      { medal: "Silver"},
-      { medal: "Bronze"}
-    ]
-    const arrayFilter = [
-      { medal: "Bronze"}
-    ]
-    expect(filterBy(lista, "medal", "Bronze")).toEqual(arrayFilter);
-  });
-
-  it('que filtre por género seleccionado y no por otro', () => {
-    const lista = [
-      { gender: "F"},  
-      { gender: "M"}
-    ]
-    const arrayFilter = [
-      { gender: "F"}
-    ]
-    expect(filterBy(lista, "gender", "F")).toEqual(arrayFilter);
-  });
-
-  it('que filtre por género seleccionado y no por otro', () => {
-    const lista = [
-      { gender: "F"},  
-      { gender: "M"}
-    ]
-    const arrayFilter = [
-      { gender: "M"}
-    ]
-    expect(filterBy(lista, "gender", "M")).toEqual(arrayFilter);
-=======
 import { filterBy, computeStat, sortAtheltes } from '../src/dataFunctions.js';
 
 describe('data', () => {
@@ -140,40 +77,8 @@ describe('data', () => {
       const expectedData = lista.slice().sort((a, b) => b.name.localeCompare(a.name)) 
       expect(sortedData).toEqual(expectedData);
     });
->>>>>>> 16ba1c1f24e31a4c215ff6bddbd77d79162fd9ed
   });
 
-<<<<<<< HEAD
-//TEST UNITARIO FUNCIÓN ORDEN
-describe('sortAthletes', () => {
-
-  it('que ordene la data ascendente según el nombre', () => {
-    const lista = [
-      {name: "Saeid Morad Abdevali"},
-      {name: "Patimat Abakarova"},
-      {name: "Luc Abalo"},
-      {name: "Giovanni Abagnale"}
-    ]
-    const sortedData = sortAtheltes(lista, "name", "asc")
-    const expectedData = lista.slice().sort((a, b) => a.name.localeCompare(b.name)) 
-    expect(sortedData).toEqual(expectedData);
-  });
-
-  it('que ordene la data descendente según el nombre', () => {
-    const lista = [
-      {name: "Saeid Morad Abdevali"},
-      {name: "Patimat Abakarova"},
-      {name: "Luc Abalo"},
-      {name: "Giovanni Abagnale"}
-    ]
-    const sortedData = sortAtheltes(lista, "name", "desc")
-    const expectedData = lista.slice().sort((a, b) => b.name.localeCompare(a.name)) 
-    expect(sortedData).toEqual(expectedData);
-  });
-});
-
-//TEST UNITARIO FUNCIÓN DE ESTADISTICA
-=======
   describe('computeStat', () => {
     const ageArray = [
       { age: 20 },
@@ -196,4 +101,3 @@ describe('sortAthletes', () => {
     });
   }); 
 })
->>>>>>> 16ba1c1f24e31a4c215ff6bddbd77d79162fd9ed
